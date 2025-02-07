e14b633c1eda710ef37a83115b142e53b85b871b
Tim Schneider <tim@robot-learning.de>
2025-02-07 21:35:39 +0100

    Reorganized __init__.py imports

--- ap_gym/__init__.py
+++ ap_gym/__init__.py
@@ -1,3 +1,8 @@
+from .active_classification_env import (
+    ActiveClassificationEnv,
+    ActiveClassificationVectorEnv,
+    CrossEntropyLossFn,
+)
 from .active_perception_env import (
     ActivePerceptionEnv,
     BaseActivePerceptionEnv,
@@ -13,22 +18,18 @@ from .active_perception_vector_env import (
     BaseActivePerceptionVectorEnv,
     ActivePerceptionVectorWrapper,
     ActivePerceptionVectorRestoreWrapper,
+    PseudoActivePerceptionVectorWrapper,
     ensure_active_perception_vector_env,
 )
-from .active_classification_env import (
-    ActiveClassificationEnv,
-    ActiveClassificationVectorEnv,
-    CrossEntropyLossFn,
-)
+from .circle_square_env import CircleSquareEnv, CircleSquareVectorEnv
+from .image_perception_vector_env import ImagePerceptionVectorEnv
+from .image_space import ImageSpace
+from .loss_fn import LossFn, LambdaLossFn, ZeroLossFn
+from .mnist_env import MNISTEnv, MNISTVectorEnv
+from .registration import make, make_vec, register_envs
 from .vector_to_single_wrapper import (
     VectorToSingleWrapper,
     ActivePerceptionVectorToSingleWrapper,
 )
-from .image_space import ImageSpace
-from .image_perception_vector_env import ImagePerceptionVectorEnv
-from .mnist_env import MNISTEnv, MNISTVectorEnv
-from .circle_square_env import CircleSquareEnv, CircleSquareVectorEnv
-from .loss_fn import LossFn, LambdaLossFn, ZeroLossFn
-from .registration import make, make_vec, register_envs
 
 register_envs()
