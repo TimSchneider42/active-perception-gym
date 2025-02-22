e42c31dda3f4f36503548443d46f264471b0335d
Tim Schneider <mail@tim-schneider.me>
2025-02-22 10:18:27 +0100

    Set default batch shape to () in LossFn

--- ap_gym/loss_fn.py
+++ ap_gym/loss_fn.py
@@ -18,22 +18,22 @@ except ImportError:
 class LossFn(Generic[PredType, PredTargetType], ABC):
     @abstractmethod
     def numpy(
-        self, prediction: PredType, target: PredTargetType, batch_shape: Tuple[int, ...]
+        self, prediction: PredType, target: PredTargetType, batch_shape: Tuple[int, ...] = ()
     ) -> float:
         pass
 
     def torch(
-        self, prediction: Any, target: Any, batch_shape: Tuple[int, ...]
+        self, prediction: Any, target: Any, batch_shape: Tuple[int, ...] = ()
     ) -> "torch.Tensor":
         raise NotImplementedError("Loss function is not implemented for torch.")
 
     def jax(
-        self, prediction: Any, target: Any, batch_shape: Tuple[int, ...]
+        self, prediction: Any, target: Any, batch_shape: Tuple[int, ...] = ()
     ) -> "jax.Array":
         raise NotImplementedError("Loss function is not implemented for torch.")
 
     def __call__(
-        self, prediction: PredType, target: PredTargetType, batch_shape: Tuple[int, ...]
+        self, prediction: PredType, target: PredTargetType, batch_shape: Tuple[int, ...] = ()
     ):
         return self.numpy(prediction, target, batch_shape)
 
@@ -50,19 +50,19 @@ class LambdaLossFn(LossFn[PredType, PredTargetType], Generic[PredType, PredTarge
         self._jax = jax
 
     def numpy(
-        self, prediction: PredType, target: PredTargetType, batch_shape: Tuple[int, ...]
+        self, prediction: PredType, target: PredTargetType, batch_shape: Tuple[int, ...] = ()
     ) -> float:
         return self._np(prediction, target, batch_shape)
 
     def torch(
-        self, prediction: Any, target: Any, batch_shape: Tuple[int, ...]
+        self, prediction: Any, target: Any, batch_shape: Tuple[int, ...] = ()
     ) -> "torch.Tensor":
         if self._torch is None:
             raise NotImplementedError("Loss function is not implemented for torch.")
         return self._torch(prediction, target, batch_shape)
 
     def jax(
-        self, prediction: Any, target: Any, batch_shape: Tuple[int, ...]
+        self, prediction: Any, target: Any, batch_shape: Tuple[int, ...] = ()
     ) -> "jax.Array":
         if self._jax is None:
             raise NotImplementedError("Loss function is not implemented for torch.")
@@ -71,16 +71,16 @@ class LambdaLossFn(LossFn[PredType, PredTargetType], Generic[PredType, PredTarge
 
 class ZeroLossFn(LossFn[Tuple, Tuple]):
     def numpy(
-        self, prediction: Tuple, target: Tuple, batch_shape: Tuple[int, ...]
+        self, prediction: Tuple, target: Tuple, batch_shape: Tuple[int, ...] = ()
     ) -> float:
         return np.zeros(batch_shape, dtype=np.float32)
 
     def torch(
-        self, prediction: Tuple, target: Tuple, batch_shape: Tuple[int, ...]
+        self, prediction: Tuple, target: Tuple, batch_shape: Tuple[int, ...] = ()
     ) -> "torch.Tensor":
         return torch.zeros(batch_shape)
 
     def jax(
-        self, prediction: Tuple, target: Tuple, batch_shape: Tuple[int, ...]
+        self, prediction: Tuple, target: Tuple, batch_shape: Tuple[int, ...] = ()
     ) -> "jax.Array":
         return jnp.zeros(batch_shape)
