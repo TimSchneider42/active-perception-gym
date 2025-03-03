864e718fd885208adb8074ba6f84495d376b5236
Tim Schneider <tim@robot-learning.de>
2025-03-03 13:30:26 +0100

    Made image and label feature names configurable in HuggingfaceImageClassificationVectorEnv

--- ap_gym/envs/huggingface_image_classification.py
+++ ap_gym/envs/huggingface_image_classification.py
@@ -24,6 +24,8 @@ class HuggingfaceImageClassificationVectorEnv(ImageClassificationVectorEnv):
         max_episode_steps: int | None = None,
         max_step_length: float = 0.2,
         prefetch: bool = True,
+        image_feature_name: str = "image",
+        label_feature_name: str = "label",
     ):
         dataset = load_dataset(
             dataset_name,
@@ -33,11 +35,13 @@ class HuggingfaceImageClassificationVectorEnv(ImageClassificationVectorEnv):
             },
         )
         self.__data = dataset[split]
+        self.__image_feature_name = image_feature_name
+        self.__label_feature_name = label_feature_name
 
         super().__init__(
             num_envs,
             len(self.__data),
-            dataset["train"].features["label"].num_classes,
+            dataset["train"].features[self.__label_feature_name].num_classes,
             render_mode=render_mode,
             max_episode_steps=max_episode_steps,
             max_step_length=max_step_length,
@@ -47,7 +51,7 @@ class HuggingfaceImageClassificationVectorEnv(ImageClassificationVectorEnv):
 
     def _load_image(self, idx: int) -> tuple[np.ndarray, int]:
         data_point = self.__data[idx]
-        img = data_point["image"]
+        img = data_point[self.__image_feature_name]
         if isinstance(img, PIL.Image.Image):
             img = np.array(img)
         assert isinstance(img, np.ndarray)
@@ -55,7 +59,7 @@ class HuggingfaceImageClassificationVectorEnv(ImageClassificationVectorEnv):
             img = img.astype(np.float32) / 255
         if len(img.shape) == 2:
             img = img[..., None]
-        return img, data_point["label"]
+        return img, data_point[self.__label_feature_name]
 
 
 def HuggingfaceImageClassificationEnv(
@@ -65,6 +69,8 @@ def HuggingfaceImageClassificationEnv(
     sensor_size: tuple[int, int] = (5, 5),
     max_episode_steps: int | None = None,
     max_step_length: float = 0.2,
+    image_feature_name: str = "image",
+    label_feature_name: str = "label",
 ):
     return ActivePerceptionVectorToSingleWrapper(
         HuggingfaceImageClassificationVectorEnv(
@@ -76,5 +82,7 @@ def HuggingfaceImageClassificationEnv(
             max_episode_steps=max_episode_steps,
             max_step_length=max_step_length,
             prefetch=False,  # Must be false as we cannot predict the seeds of the reset calls
+            image_feature_name=image_feature_name,
+            label_feature_name=label_feature_name,
         )
     )
