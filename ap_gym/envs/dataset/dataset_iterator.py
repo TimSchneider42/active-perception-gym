af3fc84a7187bab67fe9e5460a80e5e62b9a9e39
Tim Schneider <tim@robot-learning.de>
2025-03-10 21:01:49 +0100

    Implemented restrict_indices_to argument for DatasetBatchIterator

--- ap_gym/envs/dataset/dataset_iterator.py
+++ ap_gym/envs/dataset/dataset_iterator.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Generic, Any
+from typing import Iterator, Generic, Any, Sequence
 
 import numpy as np
 
@@ -13,12 +13,19 @@ class DatasetIterator(
         self,
         dataset: Dataset[DataPointType, Any],
         seed: int = 0,
+        restrict_indices_to: np.ndarray | Sequence[int] | None = None,
     ):
         self.__dataset = dataset
         self.__rng = np.random.default_rng(seed)
+        self.__restrict_indices_to = (
+            None if restrict_indices_to is None else np.asarray(restrict_indices_to)
+        )
 
     def __next__(self):
-        idx = self.__rng.integers(0, len(self.__dataset))
+        if self.__restrict_indices_to is not None:
+            idx = self.__rng.choice(self.__restrict_indices_to)
+        else:
+            idx = self.__rng.integers(0, len(self.__dataset))
         data = self.__dataset.get_data_point(idx)
         return data, idx
 
@@ -32,12 +39,19 @@ class DatasetBatchIterator(
         dataset: Dataset[Any, DataPointBatchType],
         batch_size: int = 1,
         seed: int = 0,
+        restrict_indices_to: np.ndarray | Sequence[int] | None = None,
     ):
         self.__dataset = dataset
         self.__rng = np.random.default_rng(seed)
         self.__batch_size = batch_size
+        self.__restrict_indices_to = (
+            None if restrict_indices_to is None else np.asarray(restrict_indices_to)
+        )
 
     def __next__(self):
-        idx = self.__rng.integers(0, len(self.__dataset), self.__batch_size)
+        if self.__restrict_indices_to is not None:
+            idx = self.__rng.choice(self.__restrict_indices_to, self.__batch_size)
+        else:
+            idx = self.__rng.integers(0, len(self.__dataset), self.__batch_size)
         data = self.__dataset.get_data_point_batch(idx)
         return data, idx
