d0196035c8fe4832d5046f1141a45aece2cc4b46
Tim Schneider <tim@robot-learning.de>
2025-03-10 17:57:06 +0100

    Made create_gifs.bash deterministic and added Indoor localization environments

--- tools/create_gifs.bash
+++ tools/create_gifs.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+export PYTHONPATH="$PYTHONPATH:$SCRIPT_DIR/.."
 
 mkdir -p "$SCRIPT_DIR/../doc/img"
-for env in LightDark-v0 CircleSquare-v0 CircleSquare-nograd-v0 CircleSquare-s15-v0 CircleSquare-s15-nograd-v0 CircleSquare-s20-v0 CircleSquare-s20-nograd-v0 MNIST-v0 TinyImageNet-v0 CIFAR10-v0 LIDARLocMaze-v0 LIDARLocMazeStatic-v0 TinyImageNetLoc-v0 CIFAR10Loc-v0; do
-    python "$SCRIPT_DIR/create_env_gif.py" "$env" "$SCRIPT_DIR/../doc/img/$env.gif" &
+for env in LightDark-v0 CircleSquare-v0 CircleSquare-nograd-v0 CircleSquare-s15-v0 CircleSquare-s15-nograd-v0 CircleSquare-s20-v0 CircleSquare-s20-nograd-v0 MNIST-v0 TinyImageNet-v0 CIFAR10-v0 LIDARLocMaze-v0 LIDARLocMazeStatic-v0 LIDARLocIndoor-v0 LIDARLocIndoorStatic-v0 TinyImageNetLoc-v0 CIFAR10Loc-v0; do
+  python "$SCRIPT_DIR/create_env_gif.py" "$env" "$SCRIPT_DIR/../doc/img/$env.gif" &
 done
 
 wait
