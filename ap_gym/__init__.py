e5ded62f39a08c13ce6fa0ff48ef17fedfa9889f
Tim Schneider <tim@robot-learning.de>
2025-03-11 19:23:32 +0100

    Reworked time limits to fix issues with truncation instead of termination

--- ap_gym/__init__.py
+++ ap_gym/__init__.py
@@ -2,7 +2,6 @@ from .active_classification_env import (
     ActiveClassificationEnv,
     ActiveClassificationVectorEnv,
 )
-from .active_regression_env import ActiveRegressionEnv, ActiveRegressionVectorEnv
 from .active_perception_env import (
     ActivePerceptionEnv,
     BaseActivePerceptionEnv,
@@ -21,12 +20,15 @@ from .active_perception_vector_env import (
     PseudoActivePerceptionVectorWrapper,
     ensure_active_perception_vector_env,
 )
+from .active_regression_env import ActiveRegressionEnv, ActiveRegressionVectorEnv
 from .image_space import ImageSpace
 from .loss_fn import LossFn, LambdaLossFn, ZeroLossFn, CrossEntropyLossFn, MSELossFn
-from .envs.registration import make, make_vec, register_envs
+from .time_limit import TimeLimit
 from .vector_to_single_wrapper import (
     VectorToSingleWrapper,
     ActivePerceptionVectorToSingleWrapper,
 )
+
+from .envs.registration import make, make_vec, register_envs
 
 register_envs()
--- ap_gym/envs/image_localization.py
+++ ap_gym/envs/image_localization.py
@@ -33,13 +33,11 @@ class ImageLocalizationVectorEnv(
         num_envs: int,
         image_perception_config: ImagePerceptionConfig,
         render_mode: Literal["rgb_array"] = "rgb_array",
-        max_episode_steps: int = 16,
         prefetch: bool = True,
     ):
         self.__image_perception_module = ImagePerceptionModule(
             num_envs,
             image_perception_config,
-            max_episode_steps=max_episode_steps,
             prefetch=prefetch,
         )
         if render_mode not in self.metadata["render_modes"]:
@@ -183,7 +181,7 @@ class ImageLocalizationVectorEnv(
 def ImageLocalizationEnv(
     image_perception_config: ImagePerceptionConfig,
     render_mode: Literal["rgb_array"] = "rgb_array",
-    max_episode_steps: int = 16,
+    step_limit: int = 16,
     prefetch: bool = True,
 ):
     return ActivePerceptionVectorToSingleWrapper(
@@ -191,7 +189,7 @@ def ImageLocalizationEnv(
             1,
             image_perception_config,
             render_mode=render_mode,
-            max_episode_steps=max_episode_steps,
+            step_limit=step_limit,
             prefetch=prefetch,
         )
     )
