6b4b21d75e711be58ed5dbb9317493039577a5f4
Tim Schneider <tim@robot-learning.de>
2026-05-11 13:52:33 +0200

    Increased LightDark static reward to 1.0

--- ap_gym/envs/light_dark.py
+++ ap_gym/envs/light_dark.py
@@ -131,7 +131,7 @@ class LightDarkEnv(ActiveRegressionEnv[np.ndarray, np.ndarray]):
 
         # The 0.1 is to ensure that the agent does not simply learn to terminate the episode early by moving out of
         # bounds
-        base_reward = 0.1 - 1e-3 * np.sum(action**2, axis=-1)
+        base_reward = 1.0 - 1e-3 * np.sum(action**2, axis=-1)
 
         magnitude = np.linalg.norm(action)
         if magnitude > 1:
